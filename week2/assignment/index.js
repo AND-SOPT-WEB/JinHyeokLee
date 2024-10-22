5854de8d84761ff448af88a0d884d21a5b9e5096
constantly-dev <rntqhdl10@naver.com>
2024-10-22 23:36:40 +0900

    feat: 스크롤 시 header 불투명하게 변경

--- week2/assignment/index.js
+++ week2/assignment/index.js
@@ -1,5 +1,27 @@
 import members from './data/members.js';
 
+// 스크롤 시 header 불투명하게
+const header = document.querySelector('header');
+
+const isScroll = () => {
+  if (window.scrollY > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+window.addEventListener('scroll', () => {
+  if (isScroll()) {
+    header.classList.add('on');
+  } else {
+    header.classList.remove('on');
+  }
+  window.scrollY;
+});
+
+// 초기 데이터 로딩 (localStorage 사용 아직 X)
+
 const loadData = () => {
   members.forEach((member) => {
     const tr = document.createElement('tr');
