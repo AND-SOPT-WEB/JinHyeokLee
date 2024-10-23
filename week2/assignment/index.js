343d5b6d86a6ba57557f8773f4802374c46f3773
constantly-dev <rntqhdl10@naver.com>
2024-10-24 00:19:20 +0900

    feat: modal 열리면 overlay scroll 막기

--- week2/assignment/index.js
+++ week2/assignment/index.js
@@ -175,10 +175,18 @@ searchBtn.addEventListener('click', () => {
   loadData(filteredMembers);
 });
 
+// modal module
+
 // modal open 로직
 const modalOpenBtn = document.querySelector('.modal_open_btn');
 const modal = document.querySelector('.modal');
 
 modalOpenBtn.addEventListener('click', () => {
   modal.showModal();
+  document.body.style.overflow = 'hidden'; // modal 열리면 scroll 막기
+});
+
+modal.addEventListener('close', () => {
+  console.log('close');
+  document.body.style.overflow = 'auto';
 });
