0f261ca88e63e4d26bd89d0a43c00817a8001ea8
constantly-dev <rntqhdl10@naver.com>
2024-11-05 15:51:48 +0900

    feat: 숫자 패드 클릭 로직 구현

--- week3/assignment/src/components/Game.jsx
+++ week3/assignment/src/components/Game.jsx
@@ -6,14 +6,26 @@ const Game = ({ level }) => {
   const [firstNumbers, setFirstNumbers] = useState([]);
   const [secondNumbers, setSecondNumbers] = useState([]);
   const [nextNum, setNextNum] = useState(1);
+  const [clicked, setClicked] = useState([]);
 
-  // const levelSet = {
-  //   level1: { size: 9 },
-  //   level2: { size: 16 },
-  //   level3: { size: 25 },
-  // };
+  useEffect(() => {
+    initGame();
+  }, [level]);
 
-  // Fisher-Yates 알고리즘.. -> 랜덤으로 섞는 알고리즘
+  // 게임 초기 시작 세팅
+  const initGame = () => {
+    const size = LEVEL_DATA[level].size;
+    setFirstNumbers(generateRandomNumbers(size));
+    setSecondNumbers(generateSecondNumbers(size));
+    setClicked(Array(size).fill(false));
+    setNextNum(1);
+  };
+
+  // 랜덤 숫자 생성
+  const generateRandomNumbers = (size) => shuffleArray(Array.from({ length: size }, (_, idx) => idx + 1));
+  const generateSecondNumbers = (size) => Array.from({ length: size }, (_, idx) => size + idx + 1);
+
+  // 섞는 알고리즘 (fish..뭐시기)
   const shuffleArray = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -22,31 +34,53 @@ const Game = ({ level }) => {
     return array;
   };
 
-  // array 2개 세팅 (앞, 뒤)
-  const settingArray = () => {
-    const firstArray = Array.from({ length: LEVEL_DATA[level].size }, (_, index) => index + 1);
-    const secondArray = Array.from(
-      { length: LEVEL_DATA[level].size },
-      (_, index) => LEVEL_DATA[level].size + index + 1,
-    );
-
-    const shuffledFirstArray = shuffleArray(firstArray);
-    const shuffledSecondArray = shuffleArray(secondArray);
-
-    setFirstNumbers(shuffledFirstArray);
-    setSecondNumbers(shuffledSecondArray);
+  // NumBtn 클릭 로직
+  const handleClick = (index) => {
+    if (isFirstLayer(index)) {
+      handleFirstLayerClick(index);
+    } else if (isSecondLayer(index)) {
+      handleSecondLayerClick(index);
+    }
   };
 
-  useEffect(() => {
-    settingArray();
-  }, [level]);
+  // 앞 면 클릭
+  const isFirstLayer = (index) => firstNumbers[index] === nextNum;
+
+  const handleFirstLayerClick = (index) => {
+    const updatedClicked = [...clicked];
+    updatedClicked[index] = true;
+    setClicked(updatedClicked);
+    updateNumbers(index);
+  };
+
+  // 뒷 면 클릭
+  const isSecondLayer = (index) => firstNumbers[index] > LEVEL_DATA[level].size;
+
+  const handleSecondLayerClick = (index) => {
+    const updatedNumbers = [...firstNumbers];
+    if (secondNumbers[nextNum - 1] === firstNumbers[index]) {
+      updatedNumbers[index] = null;
+      setFirstNumbers(updatedNumbers);
+    }
+  };
+
+  // 숫자 update
+  const updateNumbers = (index) => {
+    const updatedNumbers = [...firstNumbers];
+    updatedNumbers[index] = secondNumbers[nextNum - 1];
+
+    setFirstNumbers(updatedNumbers);
+    setNextNum((prev) => prev + 1);
+  };
 
   return (
     <Wrapper>
       <NextNum>다음 숫자 : {nextNum}</NextNum>
       <GamePadContainer size={LEVEL_DATA[level].size}>
-        {firstNumbers.map((num, idx) => (
-          <NumBtn key={idx}>{num}</NumBtn>
+        {firstNumbers.map((num, index) => (
+          <NumBtn key={index} onClick={() => handleClick(index)}>
+            {num}
+          </NumBtn>
         ))}
       </GamePadContainer>
     </Wrapper>
@@ -76,11 +110,9 @@ const GamePadContainer = styled.div`
 const NumBtn = styled.div`
   width: 5rem;
   height: 5rem;
-
   display: flex;
   justify-content: center;
   align-items: center;
-
   background-color: ${({ theme }) => theme.colors.blue2};
   color: ${({ theme }) => theme.colors.blue4};
   font-size: 1rem;
