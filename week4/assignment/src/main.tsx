5ef7aea96ce08028f1739734149600e348d027b0
constantly-dev <rntqhdl10@naver.com>
2024-11-11 15:27:38 +0900

    setting: GlobalStyle 설정

--- week4/assignment/src/main.tsx
+++ week4/assignment/src/main.tsx
@@ -1,6 +1,7 @@
-import { ThemeProvider } from "@emotion/react";
+import { Global, ThemeProvider } from "@emotion/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import GlobalStyle from "./styles/global.ts";
 
 import App from "./App.tsx";
 import "./index.css";
@@ -9,6 +10,7 @@ import theme from "./styles/theme.ts";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <Global styles={GlobalStyle} />
       <App />
     </ThemeProvider>
   </StrictMode>,
