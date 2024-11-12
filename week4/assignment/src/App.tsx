5baf8f947a40914912df6c55da44c129c76d4243
constantly-dev <rntqhdl10@naver.com>
2024-11-12 18:44:44 +0900

    fix: allRoutes 타입 문제 해결

--- week4/assignment/src/App.tsx
+++ week4/assignment/src/App.tsx
@@ -1,10 +1,10 @@
-import styled from "@emotion/styled";
-import { authRoutes, myPageRoutes } from "@routes";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import styled from '@emotion/styled';
+import { authRoutes, myPageRoutes } from '@routes';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
   const allRoutes = [...authRoutes, ...myPageRoutes];
-  const router = createBrowserRouter(allRoutes);
+  const router = createBrowserRouter(allRoutes as never); // 여기는 왜 문제가 생겼을까..
 
   return (
     <AppWrapper>
